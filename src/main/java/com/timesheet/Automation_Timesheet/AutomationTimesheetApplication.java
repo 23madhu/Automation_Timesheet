263b7ee1e3957a57c67a13f9953e314227afb481
23madhu <palepu.madhukiran@gmail.com>
2026-04-28 22:09:12 +0530

    Initial commit

---
+++ src/main/java/com/timesheet/Automation_Timesheet/AutomationTimesheetApplication.java
@@ -0,0 +1,11 @@
+package com.timesheet.Automation_Timesheet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication(scanBasePackages = "com.timesheet")
+public class AutomationTimesheetApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AutomationTimesheetApplication.class, args);
+    }
+}
